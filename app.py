676b730172499cde1ea19443f27a00dffd975001
Chamal Induwara <88148370+ChamalInd@users.noreply.github.com>
2026-04-07 22:08:08 +0530

    Finished messages flashing template in flask flash

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from flask_session import Session
 
 # configure app
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    # Ensure responses aren't cached
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 # configure main route
-app.route('/')
+@app.route('/')
 def index():
-    flash("Hello World")
+    flash("sup")
     return render_template('index.html')
